e60fa090eb8ef9bf3da007c68195487e1dc4a577
amelie.chambon <am.chambon@hotmail.com>
2019-02-25 14:33:32 +0100

    add QuizzThumbnail

---
+++ client/src/QuizzThumbnail.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+class QuizzThumbnail extends Component {
+   render() {
+     return (
+       <>
+         <p>{this.props.name}</p>
+
+         <img src="{this.props.icon}">
+        </>
+     );
+  }
+}
+
+export default QuizzThumbnail;
+
+
+
+
+
+q1 = array();
+<QuizzThumbnail {q1...} />
