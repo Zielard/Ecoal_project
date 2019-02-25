ab08b94c81d3d841045dec3ffac403973639da51
piotr.zielinski <piotr.zielinski@104f20.iut-lens.univ-artois.fr>
2019-02-25 13:42:06 +0100

    switch route

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,30 @@ import React, {Component} from 'react';
 import {quizzes, users} from './examples';
 import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
+import Home from "./Home.js";
+import About from "./About.js";
+
+
+function AppHeader()
+{
+
+}
 
 
 class App extends Component {
 
     render() {
 	return (
-    	  <div>
-	    Hey guys!!
-	  </div>
+
+    <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/about" component={About} />
+        <Route path="*" component={() => <p>Page Not Found</p>} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 	);
     }
 }
