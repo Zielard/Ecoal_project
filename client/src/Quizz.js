67400113d9b164d9fb3b19b47042e8fc94421f10
amelie.chambon <am.chambon@hotmail.com>
2019-02-26 12:41:14 +0100

    Correction of Quizz

--- client/src/Quizz.js
+++ client/src/Quizz.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
 import {quizzes, users} from './examples';
+import Question from "./Question.js";
 
 
 class Quizz extends Component {
@@ -13,13 +14,14 @@ class Quizz extends Component {
      console.log(this.props);
      let q = quizzes.filter(q => q._uid == this.props.match.params.id)[0];
      console.log(q);
+     let questions = this.state.q.questions.map(q => <Question {...q} />);
+                                                
      return (
        <>
        <h3>{q.name}</h3>
        <img src={HTTP_SERVER_PORT_PICTURES + q.icon}/>
-       <Link  to={'/quote/'+ q.questions[0]}>
-         <span className="button">Start</span>
-         </Link>
+       {questions}
+                                                
         </>
      );
   }
