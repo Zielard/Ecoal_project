596456325ce99aeb70c5d5714ee842d6440c77da
samuel.mohamedanli <samuel_anlihely@ens.univ-artois.fr>
2019-02-27 11:15:08 +0100

    add logo, css and update Home

--- client/src/Home.js
+++ client/src/Home.js
@@ -17,11 +17,28 @@ class Home extends Component {
     }
     render() {
       let quizz = this.state.quizzes.map(q => <QuizzThumbnail {...q} />);
+        let logo = "logosvg/logo.svg" ;
+        let person ="logosvg/person.svg" ;
+        let menu ="logosvg/menu.svg" ;  
 	return (
     <>
-    <p>HOME</p>
-    {quizz}
+    <body>
+    <div>
+        <header>
+            <div id="menu" onclick="menu()"><img src={HTTP_SERVER_PORT_PICTURES + menu} alt="menu"/></div>
+            <div id="logo"><img src={HTTP_SERVER_PORT_PICTURES + logo} alt="logo"/></div>
+            <div id="person"><img src={HTTP_SERVER_PORT_PICTURES + person} alt="person"/></div>
+        </header>
+        
+        <p id="myTitle">HOME</p>
+    </div>
+    
+    <div class="title">
+        {quizz}
+    </div>
+    </body>
     </>
+
 	);
     }
 }
